af02fb3dc4c1a76ca909b421c9fc3bf14b008f08
Jacek Dalkowski <jacekdalkowski@gmail.com>
2022-04-12 00:44:16 +0200

    Adds more of C# 6 and 8.

--- CsharpEight.cs
+++ CsharpEight.cs
@@ -7,14 +7,72 @@ namespace CsharpShowreel
     /// </summary>
     public class CsharpEight
     {
+
+        /// <summary>
+        /// Nullable reference types includes three features that help you avoid these exceptions, including the ability to explicitly mark a reference type as nullable:
+        ///  - Improved static flow analysis that determines if a variable may be null before dereferencing it.
+        ///  - Attributes that annotate APIs so that the flow analysis determines null-state.
+        ///  - Variable annotations that developers use to explicitly declare the intended null-state for a variable.
+        ///
+        /// Feature enabled by   <nullable>Enable</nullable>
+        /// </summary>
         public class NullableReferenceTypes
         {
+            public void NullStateAnalysis()
+            {
+                string message = null;
 
+                // warning: dereference null.
+                System.Console.WriteLine($"The length of the message is {message.Length}");
+
+                var originalMessage = message;
+                message = "Hello, World!";
+
+                // No warning. Analysis determined "message" is not null.
+                System.Console.WriteLine($"The length of the message is {message.Length}");
+
+                // warning!
+                System.Console.WriteLine(originalMessage.Length);
+            }
+
+            public void AttributesOnAPISignatures([System.Diagnostics.CodeAnalysis.NotNullWhen(true)] object graphToTraverse)
+            {
+
+            }
+
+            public void NullableVariableAnnotations(string? text)
+            {
+                try
+                {
+                    var nameLength1 = text.Length; // warning here
+                    var nameLength2 = text!.Length; // no warning here
+                }
+                catch
+                {
+
+                }
+            }
+
+            public NullableReferenceTypes()
+            {
+                NullStateAnalysis();
+                AttributesOnAPISignatures(null); // warning here because passing null
+                NullableVariableAnnotations(null);
+            }
         }
 
         public class RangesAndIndices
         {
-
+            public RangesAndIndices()
+            {
+                var array = new int[] { 1, 2, 3, 4, 5 };
+                var thirdItem = array[2];    // array[2]
+                var lastItem = array[^1];    // array[new Index(1, fromEnd: true)]
+                var slice1 = array[2..^3];    // array[new Range(2, new Index(3, fromEnd: true))]
+                var slice2 = array[..^3];     // array[Range.EndAt(new Index(3, fromEnd: true))]
+                var slice3 = array[2..];      // array[Range.StartAt(2)]
+                var slice4 = array[..];       // array[Range.All]
+            }
         }
 
         public class DefaultImplementationsOfInterfaceMembers
--- CsharpSix.cs
+++ CsharpSix.cs
@@ -1,5 +1,7 @@
 namespace CsharpShowreel
 {
+    using static System.String;
+
     /// <summary>
     /// C# 6 isn’t about doing more; it is about doing the same work with less code.
     /// </summary>
@@ -14,38 +16,77 @@ namespace CsharpShowreel
                 int? someStringLength = someString?.Length;
 
                 string[] someStrings = null;
-                int? secondStringLength = someStrings[1].Length;
+                int? secondStringLength = someStrings?[1].Length;
             }
         }
 
         public class ExpressionBodiedMembers
         {
+            public void DisplayName() => System.Console.WriteLine(ToString());
 
+            public override string ToString() => "ExpressionBodiedMembers";
+
+            public string SomeProperty => ToString();
         }
 
         public class StringyFeatures
         {
-
+            private string text = $"Today is {System.DateTime.Now.ToShortDateString()}";
         }
 
         public class ExceptionFilters
         {
-
+            public ExceptionFilters()
+            {
+                int val1 = 0;  
+                int val2 = 0;  
+                try  
+                {   
+                    val1 = 10; 
+                    // val2 = 0;  
+                }  
+                catch (System.Exception ex) when (val2 == 0)  
+                {  
+                    System.Console.WriteLine("Divided by zero!");  
+                }  
+                catch (System.Exception ex)  
+                {  
+                    System.Console.WriteLine(ex.Message);  
+                }  
+            }
         }
 
         public class ImportingStaticMembers
         {
-
+            public ImportingStaticMembers()
+            {
+                string someText = "";
+                System.String.IsNullOrEmpty(someText);
+                IsNullOrEmpty(someText);
+            }
         }
 
         public class ReadonlyAutomaticallyImplementedProperties
         {
+            public string Foo { get; }
 
+            public ReadonlyAutomaticallyImplementedProperties()
+            {
+                Foo = "bar";
+            }
         }
 
         public class AutomaticallyImplementedPropertiesInStructs
         {
+            struct Structure
+            {
+                public int Foo { get; private set; }
 
+                public Structure(int foo)
+                {
+                    Foo = foo;
+                }
+            }
         }
 
         public class ObjectInitializersWithIndexers
