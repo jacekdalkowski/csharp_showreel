ac92eb59298c5f61acefa6a5e46c16101053325f
Jacek Dalkowski <jacekdalkowski@gmail.com>
2022-06-19 12:22:04 +0200

    C# 9 and more on C# 8.

--- CsharpEight.cs
+++ CsharpEight.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace CsharpShowreel
@@ -67,6 +68,21 @@ namespace CsharpShowreel
                 NullStateAnalysis();
                 AttributesOnAPISignatures(null); // warning here because passing null
                 NullableVariableAnnotations(null);
+            }
+        }
+
+        public class NullCoalescingAssignment
+        {
+
+            public NullCoalescingAssignment()
+            {            
+                // You can use the ??= operator to assign the value of its right-hand operand to its left-hand operand only if the left-hand operand evaluates to null.
+                System.Collections.Generic.List<int> numbers = null;
+                int? i = null;
+
+                numbers ??= new System.Collections.Generic.List<int>();
+                numbers.Add(i ??= 17);
+                numbers.Add(i ??= 20);
             }
         }
 
@@ -153,14 +169,127 @@ namespace CsharpShowreel
             } 
         }
 
+        public class PropertyPattern
+        {
+            // A property pattern matches an expression when an expression result is non-null and every nested pattern matches the corresponding property or field of the expression result.
+            static bool IsConferenceDay(System.DateTime date) => date is { Year: 2020, Month: 5, Day: 19 or 20 or 21 };
+
+            // You can also add a run-time type check and a variable declaration to a property pattern, as the following example shows:
+            static string TakeFive(object input) => input switch
+            {
+                string { Length: >= 5 } s => s.Substring(0, 5),
+                string s => s,
+
+                System.Collections.Generic.ICollection<char> { Count: >= 5 } symbols => new string(symbols.Take(5).ToArray()),
+                System.Collections.Generic.ICollection<char> symbols => new string(symbols.ToArray()),
+
+                null => throw new System.ArgumentNullException(nameof(input)),
+                _ => throw new System.ArgumentException("Not supported input type."),
+            };
+        }
+
+        public class PositionalPattern
+        {
+            //  the type of an expression contains the Deconstruct method, which is used to deconstruct an expression result
+            public readonly struct Point
+            {
+                public int X { get; }
+                public int Y { get; }
+
+                public Point(int x, int y) => (X, Y) = (x, y);
+
+                public void Deconstruct(out int x, out int y) => (x, y) = (X, Y);
+            }
+
+            static string Classify(Point point) => point switch
+            {
+                (0, 0) => "Origin",
+                (1, 0) => "positive X basis end",
+                (0, 1) => "positive Y basis end",
+                _ => "Just a point",
+            };
+
+            // You can also match expressions of tuple types against positional patterns. In that way, you can match multiple inputs against various patterns, as the following example shows
+            static decimal GetGroupTicketPriceDiscount(int groupSize, System.DateTime visitDate)
+                => (groupSize, visitDate.DayOfWeek) switch
+                {
+                    (<= 0, _) => throw new System.ArgumentException("Group size must be positive."),
+                    (_, System.DayOfWeek.Saturday or System.DayOfWeek.Sunday) => 0.0m,
+                    (>= 5 and < 10, System.DayOfWeek.Monday) => 20.0m,
+                    (>= 10, System.DayOfWeek.Monday) => 30.0m,
+                    (>= 5 and < 10, _) => 12.0m,
+                    (>= 10, _) => 15.0m,
+                    _ => 0.0m,
+                };
+
+            public PositionalPattern()
+            {
+
+            }
+        }
+
+        public class TuplePattern
+        {
+            public static string RockPaperScissors(string first, string second)
+                => (first, second) switch
+                {
+                    ("rock", "paper") => "rock is covered by paper. Paper wins.",
+                    ("rock", "scissors") => "rock breaks scissors. Rock wins.",
+                    ("paper", "rock") => "paper covers rock. Paper wins.",
+                    ("paper", "scissors") => "paper is cut by scissors. Scissors wins.",
+                    ("scissors", "rock") => "scissors is broken by rock. Rock wins.",
+                    ("scissors", "paper") => "scissors cuts paper. Scissors wins.",
+                    (_, _) => "tie"
+                };
+        }
+
+        /// In C# 7.3 and earlier, a constructed type (a type that includes at least one type argument) can't be an unmanaged type. 
+        /// Starting with C# 8.0, a constructed value type is unmanaged if it contains fields of unmanaged types only.
+        public class UnmanagedConstructedTypes
+        {
+
+            public struct Coords<T>
+            {
+                public T X;
+                public T Y;
+            }
+
+            public UnmanagedConstructedTypes()
+            {
+                System.Span<Coords<int>> coordinates = stackalloc[]
+                {
+                    new Coords<int> { X = 0, Y = 0 },
+                    new Coords<int> { X = 0, Y = 3 },
+                    new Coords<int> { X = 4, Y = 0 }
+                };
+            }   
+        }
+
+        public class StaticLocalFunctions
+        {
+            public StaticLocalFunctions()
+            {
+                int y;
+                LocalFunction();
+
+                void LocalFunction() => y = 0;
+            }
+        }
+
         public CsharpEight()
         {
             new NullableReferenceTypes();
+            new NullCoalescingAssignment();
             new RangesAndIndices();
             // DefaultImplementationsOfInterfaceMembers
             new SwitchExpressions();
             new IAsyncEnumerable().RunIAsyncEnumerable();
             new StructReadonlyInstanceMembers(2, 4);
+            new PropertyPattern();
+            new PositionalPattern();
+            new TuplePattern();
+            new UnmanagedConstructedTypes();
+            new StaticLocalFunctions();
         }
     }
 }
--- CsharpNine.cs
+++ CsharpNine.cs
@@ -7,30 +7,137 @@ namespace CsharpShowreel
     /// </summary>
     public class CsharpNine
     {
+
+        // While records can be mutable, they are primarily intended for supporting immutable data models. The record type offers the following features:
+        // Concise syntax for creating a reference type with immutable properties
+        // Behavior useful for a data-centric reference type:
+        //  * Value equality
+        //  * Concise syntax for nondestructive mutation
+        //  * Built-in formatting for display
+        //  * Support for inheritance hierarchies
+        //
+        // You can use structure types to design data-centric types that provide value equality and little or no behavior. But for relatively large data models, structure types have some disadvantages:
+        //  * They don't support inheritance.
+        //  * They're less efficient at determining value equality. For value types, the ValueType.Equals method uses reflection to find all fields. For records, the compiler generates the Equals method. In practice, the implementation of value equality in records is measurably faster.
+        //  * They use more memory in some scenarios, since every instance has a complete copy of all of the data. Record types are reference types, so a record instance contains only a reference to the data.
         public class Records
         {
             // immutable-oriented reference types
+            public record Person_1(string FirstName, string LastName);
+
+            public record Person_2
+            {
+                public string FirstName { get; init; } = default!;
+                public string LastName { get; init; } = default!;
+            };
+
+            public record Person_3
+            {
+                public string FirstName { get; set; } = default!;
+                public string LastName { get; set; } = default!;
+            };
+
+            public Records()
+            {
+                Person_1 person1 = new Person_1("Sam", "Raynolds");
+                System.Console.WriteLine(person1); // prints out nicely
+            }
         }
 
         public class InitOnlySetters
         {
+            public System.DateTime RecordedAt { get; init; }
 
+            public InitOnlySetters()
+            {
+                RecordedAt = System.DateTime.MinValue;
+            }
         }
 
         public class TopLevelStatements
         {
-
+            // Omitting namespace and class block.
+            // Only one file in your application may use top-level statements. 
         }
 
         public class PatternMatchingEnhancements
         {
             // Conjunctive, disjunctive, negated, relational
+            public class TypePatternMatchingImprovements
+            {
+                public class Shape {}
+
+                public class Rectangle : Shape {
+                    public int Width { get; }
+
+                    public int Height { get; }
+                }
+
+                public static int Perimeter(Shape shape)
+                {
+                    if (shape is null) // new feature?
+                        throw new System.ArgumentNullException(nameof(shape));
+                    if (shape is Rectangle) // if variable is not needed, it can be skipped (we do not even need to discard it using _)
+                        return 10;
+                    throw new System.Exception();
+                }
+            }
+
+            public class ParenthesizedPattern
+            {
+                public ParenthesizedPattern(object input)
+                {
+                    if (input is not (float or double))
+                    {
+                        return;
+                    }
+                }
+            }
+
+            public class ConjunctiveDisjunctiveParenthesizedPattern
+            {
+                public static bool IsLetterOrSeparator(char c) => c is (>= 'a' and <= 'z') or (>= 'A' and <= 'Z') or '.' or ',';
+
+                public ConjunctiveDisjunctiveParenthesizedPattern(char c)
+                {
+                    if (IsLetterOrSeparator(c))
+                    {
+                        return;
+                    }
+                }
+            }
+
+            public class Relational
+            {
+                static string Classify(double measurement) => measurement switch
+                {
+                    < -4.0 => "Too low",
+                    > 10.0 => "Too high",
+                    double.NaN => "Unknown",
+                    _ => "Acceptable",
+                };
+
+                public Relational()
+                {
+
+                }
+            }
+
+            public PatternMatchingEnhancements()
+            {
+                new TypePatternMatchingImprovements();
+                new ParenthesizedPattern(new object());
+                new ConjunctiveDisjunctiveParenthesizedPattern('s');
+            }
         }
 
         public class WithExpressions
         {
-            // for records:
-            // var otherPerson = person with { LastName = "Hanselman" };
+            public WithExpressions()
+            {
+                Records.Person_1 person1 = new Records.Person_1("Sam", "Raynolds");
+                Records.Person_1 person2 = person1 with { FirstName = "John" };
+            }
         }
 
         public class DefaultImplementationsOfInterfaceMembers
